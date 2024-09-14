87d6fae1064f070dda53821ba268e1874aa577a5
JackPadalino <pada0867@gmail.com>
2024-09-14 17:15:34 -0400

    set up react navigation screens

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,9 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CategoriesScreen, MealsOverviewScreen } from "./screens";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="MealsCategories">
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{ title: "Meals Categories" }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={{ title: "Meals Overview", headerBackTitle: "Back" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({});
